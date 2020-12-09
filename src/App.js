b93058a8fd23b027da81faad5900efe82beb69b9
Robert Putt <robert.putt@rackspace.co.uk>
2020-12-09 18:51:08 +0000

    update yarn lock

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
-import React, { Component } from 'react';
-import { ThemeProvider } from '@material-ui/styles';
-import { createMuiTheme } from '@material-ui/core/styles'
+import logo from './logo.svg';
 import './App.css';
-import Routes from './routes'
-import { blue, indigo } from '@material-ui/core/colors'
 
-const theme = createMuiTheme({
-  palette: {
-    secondary: {
-      main: blue[900]
-    },
-    primary: {
-      main: indigo[700]
-    }
-  },
-  typography: {
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      '"Lato"',
-      'sans-serif'
-    ].join(',')
-  }
-});
-
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <ThemeProvider theme={theme}>
-          <Routes />
-        </ThemeProvider>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
