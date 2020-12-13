26bcfbbaeaab65eb7c5ff6b2fab0df6aa6de105d
Robert Putt <robert.putt@rackspace.co.uk>
2020-12-13 08:52:45 +0000

    Remove unused imports

--- src/components/page_documentation.js
+++ src/components/page_documentation.js
@@ -1,6 +1,5 @@
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 
--- src/components/page_home.js
+++ src/components/page_home.js
@@ -1,6 +1,5 @@
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
 import hoephoto from '../images/hoe.jpg';
 
 
